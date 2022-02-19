a9e5f275c409bba3f0a17cc0c38f24ff548d3a01
Ozgu Ozden <ozgu@theshell.co.jp>
2022-02-19 23:32:47 +0900

    Initial Commit

---
+++ Multiplier App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Multiplier App
+//
+//  Created by Ozgu Ozden on 2022/02/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Multiplier App/Multiplier_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiplier_AppApp.swift
+//  Multiplier App
+//
+//  Created by Ozgu Ozden on 2022/02/19.
+//
+
+import SwiftUI
+
+@main
+struct Multiplier_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
